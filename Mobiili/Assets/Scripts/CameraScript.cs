a27d187dd8673f3228a3f6b08b652f8134b3328f
Niklas <niklas.granqvist@hotmail.com>
2021-04-17 11:50:26 +0300

    Mobiili peli

---
+++ Mobiili/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public Transform target; //Mitä kamera seuraa
+    public Vector3 offset; //Kameran offset kohteeseen nähden
+
+    private void LateUpdate()
+    {
+        offset = new Vector3(0, 3, -8);
+        transform.position = target.position + offset; //Määritellään kameran position kohteeseen nähden
+    }
+
+
+}
---
+++ Mobiili/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 5.0f; //Eteenpäin kuljettava nopeus
+    private CharacterController characterController; //Character controller komponentti
+    private float horizontalInput; //Vakio suunta
+    private float suunta = 0; //Gyroskoopin määrittelemä suunta
+    
+
+    //Kun peli alkaa
+    void Start()
+    {
+        Input.gyro.enabled = true; //Otetaan gyroskooppin syöttö käyttöön
+        characterController = GetComponent<CharacterController>(); //Otetaan character controller käyttöön
+    }
+
+    // Ei riippuvainen frameista
+    void FixedUpdate()
+    {
+        Move();        
+    }
+
+    //Liikkeen ohjaus
+    private void Move()
+    {
+        horizontalInput = 1; //Eteenpäin liike on aina päällä
+
+        CheckDirection(); //Tarkistetaan gyroskoopin input
+        
+        
+        transform.forward = new Vector3(Mathf.Abs(horizontalInput) - 1, 0, horizontalInput); //Määritellään liikkeen suunta
+        characterController.Move(new Vector3(suunta, 0, horizontalInput * speed) * Time.deltaTime); //Liikutetaan laatikkoa
+        
+    }
+
+    //Gyroskoopin input
+    private void CheckDirection()
+    {
+
+        if (Input.gyro.rotationRate.z > 2) 
+        {
+            suunta = -10;
+        }
+
+        else if (Input.gyro.rotationRate.z < -2)
+        {
+            suunta = 10;
+        }        
+    }
+}
